0b56051169f204a8743fa86a0a6a2e747beb8ce9
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-03-25 16:22:02 +0200

    feat: ask for permissions and get the user location
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { s } from './App.style';
 import { ImageBackground } from 'react-native';
 import backgroundImage from "./assets/background.png";
 import Home from './pages/Home/Home';
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 const App = () => {
+  const [coordinates, setCoordinates] = useState();
+
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude
+      });
+    } else {
+      setCoordinates({ lat: "48.85", lng: "2.35" }); // default coordinates
+    }
+  }
   return (
     <ImageBackground imageStyle={s.image} style={s.imageBackground} source={backgroundImage}>
       <SafeAreaProvider>
