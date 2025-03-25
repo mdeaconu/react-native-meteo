89cc3dbe921d77936f305c804fc8932606de81c4
Mihai DEACONU <mihai.deaconu@cognizant.com>
2025-03-25 17:43:16 +0200

    feat: meteo basic layout
    Signed-off-by: Mihai DEACONU <mihai.deaconu@cognizant.com>

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { View } from "react-native";
 import { s } from "./Home.style";
 import Txt from "../../components/Txt/Txt";
+import MeteoBasic from "../MeteoBasic/MeteoBasic";
 
 const Home = () => {
   return (
     <>
       <View style={s.meteo_basic}>
-        <Txt style={{ fontSize: 30 }}>Basic Weather Info</Txt>
+        <MeteoBasic />
       </View >
       <View style={s.searchbar_container}>
         <Txt style={{ fontSize: 30 }}>Search Bar</Txt>
---
+++ pages/MeteoBasic/MeteoBasic.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Image, View } from "react-native";
+import { s } from "./MeteoBasic.style";
+import Txt from "../../components/Txt/Txt";
+
+const MeteoBasic = () => {
+  return (
+    <>
+      <View style={s.clock}>
+        <Txt>Clock</Txt>
+      </View>
+      <View style={s.city}>
+        <Txt>City</Txt>
+      </View>
+      <View style={s.interpretation}>
+        <Txt style={s.interpretation_txt}>Sunny</Txt>
+      </View>
+      <View style={s.temperature_box}>
+        <Txt style={s.temperature}>3º</Txt>
+        <Image style={s.image} />
+      </View>
+    </>
+  );
+};
+
+export default MeteoBasic;
